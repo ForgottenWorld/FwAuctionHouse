e34d73e1b6ba42323a234f11f85b2286f13a4bbc
Giacomo Zama <giacomozama@hotmail.it>
2021-04-12 22:32:44 +0200

    migrated to gradle

---
+++ build.gradle.kts
@@ -0,0 +1,45 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.4.32"
+    id("com.github.johnrengelman.shadow") version "6.1.0"
+}
+
+repositories {
+    mavenLocal()
+    maven("https://papermc.io/repo/repository/maven-public/")
+    maven("https://oss.sonatype.org/content/groups/public/")
+    maven("https://jitpack.io")
+    maven("https://repo.maven.apache.org/maven2/")
+}
+
+dependencies {
+    implementation("com.zaxxer:HikariCP:3.4.5")
+    implementation("org.slf4j:slf4j-simple:1.6.4")
+    compileOnly("com.destroystokyo.paper:paper-api:1.16.4-R0.1-SNAPSHOT")
+    compileOnly("com.github.MilkBowl:VaultAPI:1.7")
+}
+
+tasks.withType<ShadowJar>() {
+    relocate("com.zaxxer.hikari", "me.kaotich00.fwauctionhouse.com.zaxxer.hikari")
+    relocate("org.slf4j", "me.kaotich00.fwauctionhouse.org.slf4j")
+}
+
+group = "me.kaotich00"
+version = "0.0.1"
+description = "FwAuctionHouse"
+
+java.sourceCompatibility = JavaVersion.VERSION_1_8
+
+tasks.withType<JavaCompile> {
+    sourceCompatibility = "1.8"
+    targetCompatibility = "1.8"
+    options.encoding = "UTF-8"
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
